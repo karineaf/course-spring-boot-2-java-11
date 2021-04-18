cfd514d95d4cf929bca4784c5c52165edc76cc54
karine <karineantunesfarias@gmail.com>
2021-04-18 13:03:48 -0300

    adicionado banco de dados h2, utilizacao de jpa e criacao de perfil de teste

--- src/main/java/com/java/completo/course/entities/User.java
+++ src/main/java/com/java/completo/course/entities/User.java
@@ -1,12 +1,16 @@
 package com.java.completo.course.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
 
     public static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
