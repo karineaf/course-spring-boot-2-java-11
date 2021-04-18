a571944a1c4cbb586a2d5f834ef5bbb01bba282f
karine <karineantunesfarias@gmail.com>
2021-04-18 17:13:04 -0300

    enum status do pedido(OrderStatus)

--- src/main/java/com/java/completo/course/entities/Order.java
+++ src/main/java/com/java/completo/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.java.completo.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.java.completo.course.entities.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -20,15 +21,18 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant instant;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order(){}
 
-    public Order(Long id, Instant instant, User client) {
+    public Order(Long id, Instant instant, OrderStatus orderStatus, User client) {
         this.id = id;
         this.instant = instant;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -46,6 +50,15 @@ public class Order implements Serializable {
 
     public void setInstant(Instant instant) {
         this.instant = instant;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null)
+            this.orderStatus = orderStatus.getCode();
     }
 
     public User getClient() {
