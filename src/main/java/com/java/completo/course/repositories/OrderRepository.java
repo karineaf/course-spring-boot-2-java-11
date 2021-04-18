7de95cf7de28e6375ae4d33d57dd079474ba5ff1
karine <karineantunesfarias@gmail.com>
2021-04-18 16:16:49 -0300

    entidade pedido, utilizacao do instant e padrao para data(ISO 8601)

---
+++ src/main/java/com/java/completo/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.java.completo.course.repositories;
+
+import com.java.completo.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
