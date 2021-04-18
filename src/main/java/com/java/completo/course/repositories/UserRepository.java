760caba415b49182945de92f4c45e1d4b91b062b
karine <karineantunesfarias@gmail.com>
2021-04-18 13:48:37 -0300

    camada de serviço e registro de componente(@service)

--- src/main/java/com/java/completo/course/repositories/UserRepository.java
+++ src/main/java/com/java/completo/course/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.java.completo.course.repositories;
 
 import com.java.completo.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
