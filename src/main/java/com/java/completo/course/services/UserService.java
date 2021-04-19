1e10bd22472862e6335b5b3be41fcd9a9d35b350
karine <karineantunesfarias@gmail.com>
2021-04-19 13:45:36 -0300

    tratamento excecao de update de usuario

--- src/main/java/com/java/completo/course/services/UserService.java
+++ src/main/java/com/java/completo/course/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,10 +43,14 @@ public class UserService {
     }
 
     public User update(Long id, User user){
+        try{
         //getOne -> prepara o obj monotorado para mexer e depois enviar para o banco
         User entity = repository.getOne(id);
         updateData(entity, user);
         return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
