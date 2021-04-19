bda6e70a0c1530e5f84009cba27fba0ad89e6414
karine <karineantunesfarias@gmail.com>
2021-04-19 13:29:36 -0300

    tratamento de excecao no findbyid do usuario

--- src/main/java/com/java/completo/course/services/UserService.java
+++ src/main/java/com/java/completo/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.java.completo.course.services;
 
 import com.java.completo.course.entities.User;
 import com.java.completo.course.repositories.UserRepository;
+import com.java.completo.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> user = repository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user){
