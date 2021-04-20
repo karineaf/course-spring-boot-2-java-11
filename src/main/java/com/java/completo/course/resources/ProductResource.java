2e173c4b65d234b3f61acb363363bed47a873827
karine <karineantunesfarias@gmail.com>
2021-04-20 14:30:07 -0300

    insert, delete e update de produto

--- src/main/java/com/java/completo/course/resources/ProductResource.java
+++ src/main/java/com/java/completo/course/resources/ProductResource.java
@@ -1,14 +1,14 @@
 package com.java.completo.course.resources;
 
 import com.java.completo.course.entities.Product;
+import com.java.completo.course.entities.User;
 import com.java.completo.course.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,6 +27,25 @@ public class ProductResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Product> findById(@PathVariable Long id){
         Product product = service.findById(id);
+        return ResponseEntity.ok().body(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody Product product){
+        product = service.insert(product);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(product.getId()).toUri();
+        return ResponseEntity.created(uri).body(product);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product){
+        product = service.update(id, product);
         return ResponseEntity.ok().body(product);
     }
 }
--- src/main/java/com/java/completo/course/services/ProductService.java
+++ src/main/java/com/java/completo/course/services/ProductService.java
@@ -1,10 +1,16 @@
 package com.java.completo.course.services;
 
 import com.java.completo.course.entities.Product;
+import com.java.completo.course.entities.User;
 import com.java.completo.course.repositories.ProductRepository;
+import com.java.completo.course.services.exceptions.DatabaseException;
+import com.java.completo.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +26,39 @@ public class ProductService {
 
     public Product findById(Long id){
         Optional<Product> product = repository.findById(id);
-        return product.get();
+        return product.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Product insert(Product product){
+        return repository.save(product);
+    }
+
+    public void delete(Long id){
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Product update(Long id, Product product){
+        try{
+            //getOne -> prepara o obj monotorado para mexer e depois enviar para o banco
+            Product entity = repository.getOne(id);
+            updateData(entity, product);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Product entity, Product product) {
+        entity.setName(product.getName());
+        entity.setDescription(product.getDescription());
+        entity.setPrice(product.getPrice());
+        entity.setImgUrl(product.getImgUrl());
     }
 }
+
